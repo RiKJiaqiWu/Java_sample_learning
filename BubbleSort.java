ec10fca918ea59ffae007b791923fecef4f8fcf6
RikJiaqiWu <wujiaqi204@outlook.com>
2022-06-07 16:19:05 +0800

    6/7

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,14 +1,15 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class BubbleSort {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("请输入字符串");
+        System.out.println("请输入字符串：");
         String str = in.next();
         char[] ch = str.toCharArray();
-        for(int i = 0;i < ch.length - 1;i++ ){
-            for(int j = 0;j < ch.length - 1;j++ ){
-                if((int)ch[j] < (int)ch[j+1]){
+        for(int i = 0;i < ch.length - 1;i++ ) {
+            for(int j = 0;j < ch.length - 1;j++ ) {
+                if((int)ch[j] < (int)ch[j+1]) {
                     char temp = ch[j];
                     ch[j] = ch[j+1];
                     ch[j+1] = temp;
@@ -16,6 +17,11 @@ public class BubbleSort {
             }
         }
         String str1 = String.valueOf(ch);
-        System.out.println(str1);
+        System.out.println("有序字符串：" + str1);
+        int[] text = new int[ch.length];
+        for(int i = 0;i < ch.length;i++) {
+           text[i] = (int)ch[i];
+        }
+        System.out.println("ASCII码：" + Arrays.toString(text));
     }
 }
--- MultiTable.java
+++ MultiTable.java
@@ -3,7 +3,7 @@ public class MultiTable {
         for (int i = 1; i <= 9; i++) {
             String line = "";
             for (int j = 1; j <= 9; j++) {
-                if (i < j) {
+                if (i < j){
                     break;
                 }
                 line = line + j + "*" + i + "=" + (j * i) + "\t";
